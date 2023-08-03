831a28e9922a0cd6471858920a92decaec0e5cda
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-08-03 14:04:10 -0400

    Implement 'add color' functionality

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React, { useState } from "react"
 // Make sure to import the component we just built:
 import ColorBlock from "./Components/ColorBlock"
+import ColorForm from "./Components/ColorForm"
 import "./App.css"
 
 function App() {
-  let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+  let [colors, setColors] = useState([
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "indigo",
+    "violet",
+  ])
 
-  let colorMap = colors.map((color, i) => {
-    return <ColorBlock color={color} />
-  })
+  const addColor = (newColor) => {
+    setColors([...colors, newColor])
+  }
 
   return (
     <div className="App">
       {colors.map((color, i) => (
         <ColorBlock key={i} color={color} />
       ))}
+      <ColorForm addColor={addColor} />
     </div>
   )
 }
